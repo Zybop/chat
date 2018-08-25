7cec9214a1329652e71bcb16d4d78135d0c05024
Stian <stianrn@stud.ntnu.no>
2018-08-25 16:56:38 +0200

    Added a new scrollbar and some comments
    Also added some example text from the contact.
    Changed the color scheme to gray

--- HTML5Application/public_html/chat.js
+++ HTML5Application/public_html/chat.js
@@ -1,32 +1,38 @@
-function message() {
+function sendMessage() {
+    //create the elements
     var listItem = document.createElement("li");
     var p = document.createElement("p");
 
-    p.classList.add('text-left', 'border', 'border-light', 'textBobble');
-    listItem.style.cssFloat = "left";
+    //add class to the p element
+    p.classList.add('receiverTextBobble');
 
+    //Create a text node, with the input from the user.
     var textNode = document.createTextNode(textInputID.value);
 
-    //This makes no message if no chars is entered.
+    //Makes no message if no chars is entered in the input form.
     if (textNode.length >= 1) {
         p.appendChild(textNode);
         document.getElementById("chatListID").appendChild(p);
     }
 
+    //Clear the input form.
     textInputID.value = '';
 }
 
+// This function checks if Enter is pushed, and sends the message if it is.
 function onEnter() {
     $('#textInputID').keypress(function(e) {
         if (e.which === 13) {
-            message();
+            sendMessage();
+            scrollToBottom()
             return false;
         }
     });
 }
 
-/* function scrollToBottom() {
+//Scrolls the chat window to the latest message.
+function scrollToBottom() {
     $(document).ready(function() {
-        $('body,html').animate({ scrollTop: $(document).height() }, 800);
+        $('#chatWindow').animate({ scrollTop: $(document).height() }, 800);
     });
-} */
+}
