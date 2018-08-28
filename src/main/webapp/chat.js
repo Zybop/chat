4824c6e02d83b7c6f9a195023ba93a22db942b5e
Stian <stianrn@stud.ntnu.no>
2018-08-29 01:09:05 +0200

    Clean-up and changes to CSS
    HMTL:
    Removed some unnecessary div's and classes.
    
    CSS:
    Made the CSS look better, though this is still a mess.
    
    JS:
    Added eventListeners to check if Enter is pushed.
    Changed the way the window scrolls to the latest message.

--- src/main/webapp/chat.js
+++ src/main/webapp/chat.js
@@ -8,47 +8,60 @@ function sendMessage() {
     //Create a text node, with the input from the user.
     var textInputValue = document.getElementById("textInputID").value;
     var textNode = document.createTextNode(textInputValue);
-    
+
+    // check if input is img file
     var isPicture = false;
     if (textInputValue.includes(".jpg")) {
         isPicture = true;
+    } else if (textInputValue.includes(".png")) {
+        isPicture = true;
+    } else if (textInputValue.includes(".gif")) {
+        isPicture = true;
     }
-    else if (textInputValue.includes(".png")) {
-          isPicture = true;
-    }
-    else if (textInputValue.includes(".gif")) {
-          isPicture = true;
-    }
-    if (isPicture){
+    if (isPicture) {
         var img = document.createElement("img");
         img.setAttribute("src", textInputValue);
         img.classList.add('receiverImgBobble');
         document.getElementById("chatListID").appendChild(img);
-        
+        img.scrollIntoView(false);
+
         //TODO: add a new line here. Because right now the img appends the last message
     }
     //Checks if any chars is entered in the input form. If not, do nothing.
     else if (textNode.length >= 1) {
         p.appendChild(textNode);
         document.getElementById("chatListID").appendChild(p);
+        p.scrollIntoView(false);
     }
-   
+
 
 
     //Clear the input form.
     textInputID.value = '';
 }
 
-// This function checks if Enter is pushed, and sends the message if it is.
-function onEnter() {
-    $('#textInputID').keypress(function (e) {
-        if (e.which === 13) {
-            sendMessage();
-            scrollToBottom()
-            return false;
-        }
-    });
-}
+// This eventListener checks if Enter is pushed, and sends the message if it is.
+
+document.querySelector('#textInputID').addEventListener('keypress', function(e) {
+    var key = e.which || e.keyCode;
+    if (key === 13) {
+        sendMessage()
+        e.preventDefault();
+        e.stopPropagation();
+    }
+});
+
+// This eventListener checks if Enter is pushed, and adds a contact to the contactlist.
+
+document.querySelector('#contactInputID').addEventListener('keypress', function(e) {
+    var key = e.which || e.keyCode;
+    if (key === 13) {
+        addContact()
+        e.preventDefault();
+        e.stopPropagation();
+    }
+});
+
 function addContact() {
     var listItem = document.createElement("li");
     var contact = contactInputID.value;
@@ -60,12 +73,5 @@ function addContact() {
         listItem.appendChild(contactNode);
         document.getElementById('contacts').appendChild(listItem);
     }
-    contact = '';
-}
-
-//Scrolls the chat window to the latest message.
-function scrollToBottom() {
-    $(document).ready(function () {
-        $('#chatWindow').animate({scrollTop: $(document).height()}, 800);
-    });
+    contactInputID.value = '';
 }
